5e0875f67cd4ba304b08cfa1a66c0a88a35d29fa
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2022-03-17 01:35:48 -0700

    Add empty view component

---
+++ components/HomeEmptyView.js
@@ -0,0 +1,41 @@
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+
+
+export default function HomeEmptyView() {
+  return (
+    <View style={styles.container}>
+      <AntDesign name="addfile" size={96} color="#CBCBCB" />
+      <Text style={styles.text}>
+        Consider adding courses from our repository
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>ADD COURSE</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: '#727272',
+    padding: 20,
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#3C6BA3',
+    paddingHorizontal: 32,
+    paddingVertical: 10,
+    borderRadius: 8
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18
+  }
+});
